81b1c2d48c1322429dcb40deecae3c77614b1cd4
moa3337 <moa-x@hotmail.com>
2023-03-06 15:15:03 +0100

    setting search bar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script>
-
+import AppHeader from "./AppHeader.vue";
 import AppMain from "./AppMain.vue";
 import axios from "axios";
 
@@ -8,29 +8,39 @@ export default {
     return {
       title: "BOOLFLIX",
       series: [],
+      endpoint: "https://api.themoviedb.org/3/search/tv?api_key=c1610722c2c90e0f63ab759963e2c84c&query=movies",
       pic: "http://image.tmdb.org/t/p/w300",
     };
   },
 
-  components: { AppMain },
+  components: { AppHeader, AppMain },
 
   created() {
-    axios.get("https://api.themoviedb.org/3/search/tv?api_key=c1610722c2c90e0f63ab759963e2c84c&query=movies")
+    axios.get(this.endpoint)
       .then((response) => {
         console.log(response);
         this.series = response.data.results;
         //this.img = "http://image.tmdb.org/t/p/w300"
       });
   },
+
+  methods: {
+    fetchFilter(term) {
+      console.log(term);
+      this.fetchFilter(`${this.endpoint}?name=${term}`);
+    },
+  },
 };
 </script>
 
 <template>
-  <h1 class="text-danger fw-bold">
-    <a href="">{{ title }}</a>
-  </h1>
+  <header class="bg-dark">
+    <AppHeader placeholder="Cerca Titolo" @on-search="fetchFilter" :title="title" />
+  </header>
   <!---->
-  <AppMain />
+  <main class="bg-secondary">
+    <AppMain :TVseries="series" />
+  </main>
 </template>
 
 <style scoped></style>
--- src/AppHeader.vue
+++ src/AppHeader.vue
@@ -1,21 +1,50 @@
 <script>
+//import
 export default {
     data() {
         return {
+            term: "",
         };
     },
 
-    components: {},
+    //components: {},
+    props: {
+        placeholder: String,
+        title: String,
+    },
 
-    created() {
+    emits: ["on-search"],
+
+    methods: {
+        clear() {
+            this.term = "";
+        },
+        search() {
+            this.$emit("on-search", this.term);
+        },
     },
 };
 </script>
 
 <template>
-    <h1 class="text-danger fw-bold">
-        <a href="">{{ title }}</a>
-    </h1>
+    <div class="container d-flex justify-content-between align-items-center">
+        <h1>
+            <a class="text-danger fw-bold" href="">
+                {{ title }}
+            </a>
+        </h1>
+        <form @submit.prevent="">
+            <nav class="input-group d-flex">
+                <input type="text" class="form-control" :placeholder="placeholder || 'cerca...'" v-model="term" />
+                <button class="btn btn-outline-success" type="button" @click="search">Cerca</button>
+                <button class="btn btn-outline-danger" type="button" @click="clear">Reset</button>
+            </nav>
+        </form>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+a {
+    text-decoration: none;
+}
+</style>
