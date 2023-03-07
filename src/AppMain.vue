11b6637c74cba61e81edf71b0b97ea780837ac4f
moa3337 <moa-x@hotmail.com>
2023-03-07 16:44:06 +0100

    lista serie tv

--- src/AppMain.vue
+++ src/AppMain.vue
@@ -1,5 +1,7 @@
 <script>
 import { store } from "./data/store";
+import BaseCard from "./components/BaseCard.vue";
+
 export default {
     data() {
         return {
@@ -7,18 +9,9 @@ export default {
         };
     },
 
-    //components: {  },
+    components: { BaseCard },
     props: { TVmovies: Array, pic: String },
 
-    methods: {
-        getFalg(country) {
-            country = country.toUpperCase();
-
-            if (country == "EN") return "https://flagsapi.com/GB/flat/24.png";
-            if (country == "JA") return "https://flagsapi.com/JP/flat/24.png";
-            return "https://flagsapi.com/" + country + "/flat/24.png";
-        },
-    },
 };
 </script>
 
@@ -27,14 +20,8 @@ export default {
         <!--  -->
         <div class="row row-cols-1 row-cols-md-3 row-cols-lg-4">
             <div class="col mt-3" v-for="movie in TVmovies">
-                <img class="img-fluid" :src="pic + movie.poster_path" alt="">
-                <h4 v-if="movie.title !== 'Movies'" class="text-light">{{ movie.title }}</h4>
-                <p class="text-light">originale title: {{ movie.original_title }}</p>
-                <p class="text-light">
-                    language: ({{ }})
-                    <img :src="getFalg(movie.original_language)" alt="">
-                </p>
-                <p class="text-light">vote: {{ movie.vote_average }}</p>
+                <BaseCard :title="movie.title" :originalTitle="movie.original_title" :lang="movie.original_language"
+                    :vote="movie.vote_average" />
             </div>
         </div>
     </div>
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    props: {
+        title: String,
+        originalTitle: String,
+        lang: String,
+        vote: Number,
+    },
+
+    methods: {
+        getFalg(country) {
+            country = country.toUpperCase();
+
+            if (country == "EN") return "https://flagsapi.com/GB/flat/24.png";
+            if (country == "JA") return "https://flagsapi.com/JP/flat/24.png";
+            return "https://flagsapi.com/" + country + "/flat/24.png";
+        },
+    },
+}
+</script>
+
+<template>
+    <div class="col">
+        <img class="img-fluid" :src="pic + movie.poster_path" alt="">
+        <h4 v-if="movie.title !== 'Movies'" class="text-light">{{ title }}</h4>
+        <p class="text-light">originale title: {{ originalTitle }}</p>
+        <p class="text-light">
+            language: ()
+            <img :src="getFalg(lang)" alt="">
+        </p>
+        <p class="text-light">vote: {{ vote }}</p>
+    </div>
+</template>
